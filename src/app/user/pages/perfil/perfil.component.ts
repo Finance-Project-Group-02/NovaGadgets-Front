9b3ae698af8f98430c9174e2a8c7f0ce299eba5e
farioraro <carlos8a29@hotmail.com>
2024-11-11 22:40:29 -0500

    profile updated

--- src/app/user/pages/perfil/perfil.component.ts
+++ src/app/user/pages/perfil/perfil.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login/login.service';
 import { User } from '../../models/user.model';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatSelectModule} from '@angular/material/select';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
+  standalone: true,
+  imports: [
+    MatIconModule, MatFormFieldModule, MatSelectModule, MatButtonModule, CommonModule
+  ],
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
-
   user?: User | null;
-
-  constructor(private loginService: LoginService) { 
-
+  constructor(private loginService: LoginService) {
   }
 
   ngOnInit() {
@@ -22,5 +28,4 @@ export class PerfilComponent implements OnInit {
     }
     );
   }
-
 }
